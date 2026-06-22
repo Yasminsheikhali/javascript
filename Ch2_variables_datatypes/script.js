2293cd256b3aa9c8144b6dc322a208ec6aa4fb0d
Yasmin <yasminsheikhali63@gmail.com>
2026-06-22 23:12:43 +0100

    first commit

---
+++ Ch2_variables_datatypes/script.js
@@ -0,0 +1,22 @@
+var town ="london" // string
+// console.log(town)
+// var firstName ="Yasmin" // string
+// console.log(firstName)
+// var lastName ="SheikhAli" // string
+// console.log(lastName)
+
+var age = 30 // number
+// console.log (age) 
+
+// var isStudent = true; // boolean
+// console.log (isStudent)
+
+// console.log(age +5)
+
+// console.log (firstName * lastName)
+
+
+// var theNameOfTheManWhoDrivesTheRedCar = "ali"
+
+// console.log(typeof age)
+console.log(typeof town)
