cab58a42af9984d47bb42e6bd84d9e8b6e85ffe6
Yasmin <yasminsheikhali63@gmail.com>
2026-07-25 11:11:08 +0100

    I learnt about Objects

---
+++ Ch11_Objects/script.js
@@ -0,0 +1,45 @@
+// let person = {
+// name: "John Doe",
+// age: 30,
+// city: "New York",
+// saySalaam: function() {
+//     console.log("Asalaam Alaikum")
+// }
+// }
+
+// person.saySalaam()
+// // person.age = 50
+// // person. name = "Jane Doe"   
+// // console.log(person["age"])
+
+
+// let {city} = person
+// console.log(city)
+
+
+
+
+let vehicle = {
+    brand: "Toyota",
+    model: "Camry",
+    year: 2020,
+    color: "red",
+    // owner: person,/
+    stop: function() {
+        console.log("the car has stopped")
+    }
+}
+console.log(vehicle.stop())
+// // console.log (person.name)
+// console.log (vehicle.owner.name)
+
+// let animal = new Object();
+// console.log(animal)
+// animal.type = "Dog";
+// animal.breed = "golden Retriever";
+// console.log(animal)
+
+// let animal = {}
+// animal.type = "Dog"
+// animal.breed = "golden Retriever"
+// console.log(animal)
